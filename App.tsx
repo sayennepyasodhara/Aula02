4f3cd97731c5ccbe4bb8cc3bbe5c3038ebc1b785
sayenne <sayennepyasodhara@gmail.com>
2021-09-23 18:27:23 -0400

    imagem

--- App.tsx
+++ App.tsx
@@ -1,108 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, ProgressViewIOSComponent} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-const OneLinkHeader = (props:any)=>{
-  return(
-    <View> 
-      <Text style={StyleOneLinkHeader.link}>{ProgressViewIOSComponent.nome}</Text>
-    </View>
-  )
-};
-
-const LinksHeader = ()=> {
-  return( /* Aui dentro eu colo meu JSX */
-    <View style={StyleLinksHeader.container}> 
-     {/*  <Text style ={{color: "#4A4A4C"}}>Home</Text>
-      <Text style={StyleLinksHeader.link}>Post</Text>
-      <Text>Videos</Text>
-      <Text>Photos</Text>
-      <Text>Community</Text> */}
-      <OneLinkHeader nome="Home"/>
-      <OneLinkHeader nome="Post"/>
-      <OneLinkHeader nome="Videos"/>
-      <OneLinkHeader nome="Photos"/>
-      <OneLinkHeader nome="Community"/>
-
-
-      <OneLinkHeader/>
+const OneLinkHeader = (props: any) => {
+  return (
+    <View>
+      <Text style={styleOneLinkHeader.link}>{props.nome}</Text>
     </View>
   );
 };
-const StyleOneLinkHeader = StyleSheet.create({
-  link:{
-    color: "#4A4A4C"
-  },
- });
+const styleOneLinkHeader = StyleSheet.create({
+  link: {
+    color: "#4A4A4C",
+  }
+});
 
-const StyleLinksHeader = StyleSheet.create({
-  container:{
-/*      backgroudColor:"lightblue", */ 
-     color:"white",
-     flexDirection:"row",
-     justifyContent:"space-between",
-     paddingHorizontal:35,
-     paddingVertical:10,
-     borderBottomColor:"#f0f0f0",
-     borderBottomWidth:0.5,
-  },
- });
+const LinksHeader = () => {
+  return (// Aqui dentro eu coloco meu JSX
+    <View style={styleLinksHeader.container}>
+      {/* <Text style={{color:"#4A4A4C"}}>Home</Text>
+            <Text style={styleLinksHeader.link}>Post</Text>
+            <Text>Videos</Text>
+            <Text>Photos</Text>
+            <Text>Community</Text> */}
+      <OneLinkHeader nome="Home" />
+      <OneLinkHeader nome="Post" />
+      <OneLinkHeader nome="Videos" />
+      <OneLinkHeader nome="Photos" />
+      <OneLinkHeader nome="Community" />
+    </View>
+  );
+};
 
-export default function App() {
+const styleLinksHeader = StyleSheet.create({
+  container: {
+    //backgroundColor:"lightblue",
+    color: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 35,
+    paddingVertical: 10,
+    borderBottomColor: "#F0F0F0",
+    borderBottomWidth: 0.5,
+  },
+});
+
+const Home = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-            <Image source={
-                require("./src/assets/imagem1.png")}></Image>
-           <View style={styles.inputTextView}> 
-         <Image source={
-                require("./src/assets/imagem2.png")}></Image>
-               <TextInput 
-                 style={styles.textInput}
-                 placeholder="Search"
-                 placeholderTextColor="#F5FFFF"
-                 >  
+        <Image source={require("./src/assets/esquerd.png") }> </Image>
+        <View style={styles.inputTextView}>
+          <Image source={
+            require("./srcs/assets/direit.png")
+          }>
+          </Image>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Search"
+            placeholderTextColor="#F5FFFF"
+          >
 
 
-                 </TextInput>
-          </View>
-              <Image source={
-                require("./src/assets/imagem3.png")}></Image> 
-                 
-                </View>
-       <LinksHeader></LinksHeader>
+          </TextInput>
+        </View>
+        <Image source={
+          require("./srcs/assets/lup.png")
+        }>
+        </Image>
+      </View>
+      <LinksHeader></LinksHeader>
+
+
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#FFF',
   },
-  header:{
-    minHeight:70,
-    backgroudColor:"#4369B0",
-    paddingVertical:30, 
-    paddingTop:50,
-    flexDirection:"row",
-    justifyCountent:"space-batween",
-    alignItem:"center",
-    paddingHorizontal:30,
-   
+  header: {
+    minHeight: 70,
+    backgroundColor: "#4369B0",
+    paddingVertical: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingTop: 50,
   },
-  inputTextView:{
-      borderRadius:28,
-      backgroundColor:"#2c4877",
-      height:40, 
-      alignItems: "center",
-      flexDirection:"row",
+  inputTextView: {
+    flex: 1,
+    borderRadius: 28,
+    backgroundColor: "#2C4877",
+    marginHorizontal: 10,
+    height: 40,
+    alignItems: "center",
+    flexDirection: "row",
   },
-  textInput:{
-     flex:1,
-     paddingRight:10,
-     color:"#F5FFFF",
+  textInput: {
+    flex: 1,
+    paddingRight: 10,
+    color: "#F5FFFF",
   },
 });
+
